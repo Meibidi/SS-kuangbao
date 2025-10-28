10c77ad5fad0379629261648820078b2c7f75e04
Meibidi <ss@zzko.cn>
2025-10-28 14:21:52 +0800

    Update _worker(压缩版).js

--- _worker(压缩版).js
+++ _worker(压缩版).js
@@ -1,23 +1,4 @@
 import{connect as C}from'cloudflare:sockets'
-const W=new Map,U='', //UUID
-M=new Uint8Array(32768),Q=[],E=[new Response(null,{status:400}),new Response(null,{status:502})],
-A=s=>{const o=19+s[17],p=s[o]<<8|s[o+1],b=o+3,y=s[o+2]&1;return[y?s[b]+'.'+s[b+1]+'.'+s[b+2]+'.'+s[b+3]:new TextDecoder().decode(s.subarray(b+1,b+1+s[b])),p,y?b+4:b+1+s[b]]},
-B=(h,p)=>{try{const c=C({hostname:h,port:p});return c.opened.then(()=>c,()=>0)}catch{return Promise.resolve(0)}},
-V=(s,u)=>{for(let i=0,j=0;i<16;i++,j+=2+(j==6||j==11||j==16||j==21?1:0))if(s[i+1]!==parseInt(u.substr(j,2),16))return 1}
-let m=0,n=0
-export default{async fetch(r){
-if(r.headers.get('upgrade')!=='websocket')return E[1]
-const h=r.headers.get('sec-websocket-protocol');if(!h)return E[0]
-const d=atob(h.replace(/[-_]/g,x=>x<'.'?'+':'/')),z=d.length;if(z<18)return E[0]
-const t=m+z<32768,s=t?new Uint8Array(M.buffer,m,m+=z):n?Q[--n]||new Uint8Array(z):new Uint8Array(z),R=()=>{t?m>24576&&(m=0)||(m-=z):n<12&&!Q[n]&&(Q[n++]=s)}
-for(let i=z;i--;)s[i]=d.charCodeAt(i)
-if(s[0]||V(s,U)){R();return E[0]}
-const[x,p,e]=A(s),k=await B(x,p)||await B('proxy.xxxxxxxx.tk',50001);if(!k){R();return E[1]} //Proxyip
-const{0:c,1:w}=new WebSocketPair,g=k.writable.getWriter(),f=[1,0],D=()=>{try{w.close(f[1])}catch{};try{k.close()}catch{};W.delete(w);W.size>999&&W.clear()},L=()=>f[0]&&(f[0]=0,g.releaseLock(),D())
-w.accept();W.set(w,f);z>e&&g.write(s.subarray(e)).catch(()=>f[0]=0);R()
-w.addEventListener('message',v=>f[0]&&g.write(v.data).catch(()=>f[0]=0))
-w.addEventListener('close',()=>{f[1]=1e3;L()})
-w.addEventListener('error',()=>{f[1]=1006;L()})
-k.readable.pipeTo(new WritableStream({write(a){f[0]&&w.send(a)},close(){f[1]=1e3;L()},abort(){f[1]=1006;L()}})).catch(()=>{})
-return new Response(null,{status:101,webSocket:c})
-}}
+const W=new Map,UUID='',U=new Uint8Array(UUID.replace(/-/g,'').match(/.{2}/g).map(x=>parseInt(x,16))),M=new Uint8Array(32768),P=new Array(12),E=[new Response(null,{status:400}),new Response(null,{status:502})],A=s=>{const o=19+s[17],p=s[o]<<8|s[o+1],b=o+3,y=s[o+2]&1;return[y?s[b]+'.'+s[b+1]+'.'+s[b+2]+'.'+s[b+3]:new TextDecoder().decode(s.subarray(b+1,b+1+s[b])),p,y?b+4:b+1+s[b],s[0]]},B=(h,p)=>{const s=C({hostname:h,port:p});return s.opened.then(()=>s,()=>0).catch(()=>0)}
+let m=0,l=0
+export default{async fetch(r){if(r.headers.get('upgrade')!=='websocket')return E[1];const h=r.headers.get('sec-websocket-protocol');if(!h)return E[0];const d=atob(h.replace(/[-_]/g,x=>x<'.'?'+':'/')),n=d.length;if(n<18)return E[0];const t=m+n<32768,s=t?new Uint8Array(M.buffer,m,m+=n):l?P[--l]||new Uint8Array(n):new Uint8Array(n),F=()=>{t?m>24576&&(m=0)||(m-=n):l<12&&!P[l]&&(P[l++]=s)};for(let i=n;i--;)s[i]=d.charCodeAt(i);if(s[0]){F();return E[0]}for(let i=16;i--;)if(s[i+1]^U[i]){F();return E[0]}const[x,p,z,v]=A(s),k=await B(x,p)||await B('proxy.xxxxxxxx.tk',50001);if(!k){F();return E[1]}const{0:c,1:ws}=new WebSocketPair,w=k.writable.getWriter(),f=[1,0],H=new Uint8Array([v,0]);let g=H;ws.accept();W.set(ws,f);n>z&&w.write(s.subarray(z)).catch(()=>f[0]=0);F();const L=()=>f[0]&&(f[0]=0,w.releaseLock(),(()=>{try{ws.close(f[1])}catch{};try{k.close()}catch{};W.delete(ws);W.size>999&&W.clear()})());ws.addEventListener('message',e=>f[0]&&w.write(e.data).catch(()=>f[0]=0));ws.addEventListener('close',()=>{f[1]=1e3;L()});ws.addEventListener('error',()=>{f[1]=1006;L()});k.readable.pipeTo(new WritableStream({async write(d){if(f[0]){if(g){ws.send(await new Blob([g,d]).arrayBuffer());g=null}else ws.send(d)}},close(){f[1]=1e3;L()},abort(){f[1]=1006;L()}})).catch(()=>{});return new Response(null,{status:101,webSocket:c})}}
